811b1cb9a316a2dfa6edaecbf72503d29fccd70e
Christopher Ksiazak <cksiazak@gmail.com>
2019-10-11 19:42:07 -0600

    added login form, rerouting to bubble-page

--- client/src/App.js
+++ client/src/App.js
@@ -1,19 +1,21 @@
-import React, { useState } from "react";
-import { BrowserRouter as Router, Route } from "react-router-dom";
+import React, { useState } from 'react';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 
-import Login from "./components/Login";
-import "./styles.scss";
+import Login from './components/Login';
+import BubblePage from './components/BubblePage';
+
+import PrivateRoute from './utils/PrivateRoute';
+import './styles.scss';
+
 
 function App() {
   const [colorList, setColorList] = useState([]);
   return (
     <Router>
       <div className="App">
+
         <Route exact path="/" component={Login} />
-        {/* 
-          Build a PrivateRoute component that will 
-          display BubblePage when you're authenticated 
-        */}
+        <PrivateRoute path="/bubble-page" component={BubblePage} />
       </div>
     </Router>
   );
